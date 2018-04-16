f952591a0a82db7e9ed395e46fe1c9df7d54438a
liujie8 <cjsliuj@163.com>
2018-04-16 16:28:19 +0800

    1

---
+++ DLANControlPointService/AAA.m
@@ -0,0 +1,15 @@
+//
+//  AAA.m
+//  DLANControlPointService
+//
+//  Created by 刘杰 on 2018/4/16.
+//  Copyright © 2018年 jerry. All rights reserved.
+//
+
+#import "AAA.h"
+#import "XXX.h"
+@implementation AAA
+- (void)aaa{
+    [[XXX alloc]init];
+}
+@end
---
+++ XXX/XXX.m
@@ -0,0 +1,13 @@
+//
+//  XXX.m
+//  XXX
+//
+//  Created by 刘杰 on 2018/4/16.
+//  Copyright © 2018年 jerry. All rights reserved.
+//
+
+#import "XXX.h"
+
+@implementation XXX
+
+@end
