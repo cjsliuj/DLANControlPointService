97b64cd0daba25d739f75385492047f6524f4d68
liujie8 <cjsliuj@163.com>
2018-04-16 16:05:24 +0800

    Initial Commit

---
+++ DLANControlPointService/DLANControlPointService.h
@@ -0,0 +1,19 @@
+//
+//  DLANControlPointService.h
+//  DLANControlPointService
+//
+//  Created by 刘杰 on 2018/4/16.
+//  Copyright © 2018年 jerry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DLANControlPointService.
+FOUNDATION_EXPORT double DLANControlPointServiceVersionNumber;
+
+//! Project version string for DLANControlPointService.
+FOUNDATION_EXPORT const unsigned char DLANControlPointServiceVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DLANControlPointService/PublicHeader.h>
+
+
